c52fd734dc68aa634cd7ad6073222a5bb248055e
Jefferson Henrique <jhsg.dev@gmail.com>
2023-04-12 16:01:26 -0300

    Adicionando terceirizado
    Pensando na possibilidade de ter um terceirizado no sistema foi adicionado uma classe para o mesmo.
    E analisando que embora um terceiro tenha algumas propriedades em comum de um funcionário, mas um terceiro não precisa ter os comportamento de atualizarSalario e nem de promover. Para isso então no lugar de usar herança foi utilizado composição, onde foi extraido os dados em comum de um funcionário e de um terceiro e passados para um classe na qual pode ser utilizada por ambos.

---
+++ src/main/java/br/com/jhsgdev/rh/model/Terceirizado.java
@@ -0,0 +1,27 @@
+package br.com.jhsgdev.rh.model;
+
+public class Terceirizado {
+
+    private DadosPessoais dadosPessoais;
+    private String empresa;
+
+    public Terceirizado(DadosPessoais dadosPessoais) {
+        this.dadosPessoais = dadosPessoais;
+    }
+
+    public DadosPessoais getDadosPessoais() {
+        return dadosPessoais;
+    }
+
+    public void setDadosPessoais(DadosPessoais dadosPessoais) {
+        this.dadosPessoais = dadosPessoais;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+}
--- src/main/java/br/com/jhsgdev/rh/service/ReajusteService.java
+++ src/main/java/br/com/jhsgdev/rh/service/ReajusteService.java
@@ -17,7 +17,7 @@ public class ReajusteService {
     public void reajustarSalarioFunciionario(Funcionario funcionario, BigDecimal aumento){
         this.validacoes.forEach(v -> v.validar(funcionario, aumento));
 
-        BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
+        BigDecimal salarioReajustado = funcionario.getDadosPessoais().getSalario().add(aumento);
         funcionario.atualizaSalario(salarioReajustado);
     }
 }
